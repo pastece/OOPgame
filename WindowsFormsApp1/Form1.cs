a480c80a0cf65839b209e163fcb796d93a915cae
Yusuf Aktaş <aktass.yusuff01@gmail.com>
2022-03-25 13:19:43 +0300

    login ve setting screen

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "admin" && textBox2.Text == "admin")
+            {
+                mainGame skip = new mainGame();
+                this.Hide();
+                skip.Show();
+            }
+            else
+            {
+                MessageBox.Show("Kullanıcı adı hatalı. Tekrar deneyiniz.");
+                textBox1.Clear();
+                textBox2.Clear();
+            }
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1.PerformClick();
+            }
+        }
+
+        
+    }
+}
---
+++ WindowsFormsApp1/Settings.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Settings : Form
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+
+        private void customBtn_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            label2.Visible = true;
+            textBox1.Visible = true;
+            textBox2.Visible = true;
+            confirmBtn.Visible = true;
+        }
+
+        private void backBtn_Click(object sender, EventArgs e)
+        {
+            mainGame skip = new mainGame();
+            this.Hide();
+            skip.Show();
+        }
+    }
+}
