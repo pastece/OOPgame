3793a95a83dc56e4c175cb4510bf20fb069ad244
OmerKurtuldu <omerkurtuldu11@gmail.com>
2022-04-03 16:55:53 +0300

    Kullanıcı bilgileri kaydedildi ekrana yazdırıldı

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -41,29 +41,26 @@ namespace WindowsFormsApp1
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(332, 187);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Location = new System.Drawing.Point(249, 152);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(237, 22);
+            this.textBox1.Size = new System.Drawing.Size(179, 20);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(332, 258);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Location = new System.Drawing.Point(249, 210);
             this.textBox2.Name = "textBox2";
             this.textBox2.PasswordChar = '*';
-            this.textBox2.Size = new System.Drawing.Size(237, 22);
+            this.textBox2.Size = new System.Drawing.Size(179, 20);
             this.textBox2.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(171, 183);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(128, 149);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(113, 25);
+            this.label1.Size = new System.Drawing.Size(91, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "Username :";
             // 
@@ -71,10 +68,9 @@ namespace WindowsFormsApp1
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label2.Location = new System.Drawing.Point(172, 262);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(129, 213);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(114, 25);
+            this.label2.Size = new System.Drawing.Size(90, 20);
             this.label2.TabIndex = 3;
             this.label2.Text = "Password : ";
             // 
@@ -83,20 +79,18 @@ namespace WindowsFormsApp1
             this.BoardGame.AutoSize = true;
             this.BoardGame.Font = new System.Drawing.Font("Viner Hand ITC", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BoardGame.ForeColor = System.Drawing.SystemColors.MenuHighlight;
-            this.BoardGame.Location = new System.Drawing.Point(286, 24);
-            this.BoardGame.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.BoardGame.Location = new System.Drawing.Point(214, 20);
             this.BoardGame.Name = "BoardGame";
-            this.BoardGame.Size = new System.Drawing.Size(307, 78);
+            this.BoardGame.Size = new System.Drawing.Size(246, 61);
             this.BoardGame.TabIndex = 4;
             this.BoardGame.Text = "Board Game";
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button1.Location = new System.Drawing.Point(333, 321);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(250, 261);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(237, 36);
+            this.button1.Size = new System.Drawing.Size(178, 29);
             this.button1.TabIndex = 5;
             this.button1.Text = "Log in";
             this.button1.UseVisualStyleBackColor = true;
@@ -105,10 +99,9 @@ namespace WindowsFormsApp1
             // btnExitForm1
             // 
             this.btnExitForm1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.btnExitForm1.Location = new System.Drawing.Point(332, 409);
-            this.btnExitForm1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnExitForm1.Location = new System.Drawing.Point(249, 332);
             this.btnExitForm1.Name = "btnExitForm1";
-            this.btnExitForm1.Size = new System.Drawing.Size(237, 36);
+            this.btnExitForm1.Size = new System.Drawing.Size(178, 29);
             this.btnExitForm1.TabIndex = 6;
             this.btnExitForm1.Text = "Exit";
             this.btnExitForm1.UseVisualStyleBackColor = true;
@@ -117,10 +110,9 @@ namespace WindowsFormsApp1
             // buttonRegister
             // 
             this.buttonRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.buttonRegister.Location = new System.Drawing.Point(332, 365);
-            this.buttonRegister.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonRegister.Location = new System.Drawing.Point(249, 297);
             this.buttonRegister.Name = "buttonRegister";
-            this.buttonRegister.Size = new System.Drawing.Size(237, 36);
+            this.buttonRegister.Size = new System.Drawing.Size(178, 29);
             this.buttonRegister.TabIndex = 7;
             this.buttonRegister.Text = "Sign up";
             this.buttonRegister.UseVisualStyleBackColor = true;
@@ -128,11 +120,11 @@ namespace WindowsFormsApp1
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Info;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(919, 551);
+            this.ClientSize = new System.Drawing.Size(689, 448);
             this.Controls.Add(this.buttonRegister);
             this.Controls.Add(this.btnExitForm1);
             this.Controls.Add(this.button1);
@@ -142,7 +134,6 @@ namespace WindowsFormsApp1
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.KeyPreview = true;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
@@ -152,15 +143,14 @@ namespace WindowsFormsApp1
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label BoardGame;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button btnExitForm1;
         private System.Windows.Forms.Button buttonRegister;
+        public System.Windows.Forms.TextBox textBox1;
+        public System.Windows.Forms.TextBox textBox2;
     }
 }
 
---
+++ WindowsFormsApp1/managerScreen.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class managerScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtGridList = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dtGridList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtGridList
+            // 
+            this.dtGridList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtGridList.Location = new System.Drawing.Point(76, 21);
+            this.dtGridList.Name = "dtGridList";
+            this.dtGridList.Size = new System.Drawing.Size(655, 299);
+            this.dtGridList.TabIndex = 0;
+            // 
+            // managerScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dtGridList);
+            this.Name = "managerScreen";
+            this.Text = "managerScreen";
+            this.Load += new System.EventHandler(this.managerScreen_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtGridList)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtGridList;
+    }
+}
